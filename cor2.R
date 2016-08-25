8351d5767505254f7467a3e820581c211046b8ae
Angela P. Cuervo-Robayo <ancuervo@gmail.com>
2016-08-25 13:26:33 -0500

    Update Cor2.R

--- cor2.R
+++ cor2.R
@@ -1,4 +1,3 @@
-
 # ajuste su directorio de trabajo
 setwd('~/Bioclimas');
 
@@ -11,26 +10,20 @@ i = 1;
 while (i <= length(variables)) {
 	# elimina las primeras lineas de texto del ascii
 	values = scan(file=paste(variables[i],'.asc',sep=""),skip=6);
-	
 	values[values == -9999] = NA;
-
 	if (i == 1) {
 		df = data.frame(values);
 	} else {
 		df = data.frame(df,values);
 	}
-
 	i = i + 1;
 }
 # nombrar las columnas con el nombre de los bioclimas
 names(df) = variables;
-
 corr.matrix = cor(df,use='complete.obs');
-
-write.csv(corr.mtrx,file="corr.matrix.csv",quote=FALSE)
+write.csv(corr.matrix,file="corr.matrix.csv",quote=FALSE)
 
 # Plot de las correlaciones
-
 library(corrplot)
 corr.plot <- cor(corr.matrix)
 corrplot(corr.plot, method="circle")
